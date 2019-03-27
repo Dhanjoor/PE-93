b3d947f88370c0872af658312d769ac8066f82a2
tazalapizza <44235349+tazalapizza@users.noreply.github.com>
2019-03-27 17:20:42 +0100

    minor changes

--- Simulation/Main.py
+++ Simulation/Main.py
@@ -9,6 +9,36 @@ class Master:
         self.Humans=[]
         self.Zombies=[]
 
+    def genSound(self,x0,y0,volume):
+        if x0<0 or x0>=xSize or y0<0 or y0>=ySize or self.Map[x0][y0].content==2:
+            print("error")
+            return()
+
+        self.Map[x0][y0].sound+=volume
+        if volume==1:
+            return()
+        moves=[(0,1), (0,-1), (1,0), (-1,0), (-1,-1), (-1,1), (1,-1), (1,1)]
+        suivants=[(x0,y0,volume)]
+
+        visited=[[False for _ in range(2*volume+1)] for _ in range(2*volume+1)]
+        visited[volume][volume]=True
+
+        while suivants:
+            new=[]
+            for a,b,M in suivants:
+                for dx,dy in moves:
+                    x,y=a+dx,b+dy
+                    value=M-(dx**2+dy**2)**(1/2)
+                    if x>=0 and x<xSize and y>=0 and y<ySize and self.Map[x][y].content!=2 and not(visited[x-x0+volume][y-y0+volume]) and value>0.5:
+                        if self.Map[x][y].content==1:
+                            value-=attenuationPorte
+                            if value<=0.5:
+                                continue
+                        self.Map[x][y].sound+=round(value)
+                        visited[x-x0+volume][y-y0+volume]=True
+                        new.append((x,y,value))
+            suivants=new[:]
+
 Master=Master()
 
 #Map creation
@@ -26,19 +56,25 @@ for i in range(xSize):
         Master.Map[i][j].content=int(cell[2])
 
 for _ in range(nZombies):
-    Master.Zombies.append(create_zombie(Master))
+    Master.Zombies.append(createZombie(Master))
 
 for _ in range(nHumans):
-    Master.Humans.append(create_human(Master))
+    Master.Humans.append(createHuman(Master))
 
 #Simulation
-t=1
+t=0
 events=[(randint(0, xSize-1),randint(0,ySize-1),5)]
 with open("Map/Save.txt","w") as f:
     pass
 
 while t<=Tsimulation:
     print("======== Tour {} ========".format(t))
+
+    for x in range(xSize):
+        for y in range(ySize):
+            if Master.Map[x][y].sound>0:
+                Master.Map[x][y].sound-=1
+
     for nh in range(len(Master.Humans)-1, -1, -1):
         h=Master.Humans[nh]
         h.action()
@@ -46,10 +82,17 @@ while t<=Tsimulation:
     print()
     for nz in range(len(Master.Zombies)-1,-1,-1):
         z=Master.Zombies[nz]
-        z.action()
+        print(z.action())
         z.info()
         print(z.lifespan)
     print()
+
+    #Decrease the lifespan of the zombies
+    for nz in range(len(Master.Zombies)-1,-1,-1):
+        if Master.Zombies[nz].lifespan==1:
+            Master.Zombies[nz].death()
+        else:
+            Master.Zombies[nz].lifespan-=1
 
     #Sauvegarde
     with open("Map/Save.txt","a") as f:
