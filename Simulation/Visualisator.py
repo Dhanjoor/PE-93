cec882a47a37122e5360647e76d04c3775885152
tazalapizza <44235349+tazalapizza@users.noreply.github.com>
2019-03-27 17:43:11 +0100

    moved visu and mapedi

---
+++ Simulation/Visualisator.py
@@ -0,0 +1,155 @@
+from tkinter import *
+import numpy
+
+class Visualisator(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+
+        # Data
+        self.x_size=0   # In cells
+        self.y_size=0    # In cells
+        self.ppc=0         # pixel per cell
+        self.onclic='w'    # what to do when the user clic on a cell
+        self.nclic = 1     # first or second clic for a new wall of line
+        self.firstcase = (0,0)     # first clic for a wall of line
+        self.grid=[]
+        self.canvas=0
+        self.timer = 0
+        self.nbati=1
+        self.batiments=[]
+        self.humains=[]
+        self.zombies=[]
+        # Each cell is [x,y,z] where y is noise level and z is wall/not wall
+
+        # Load map
+        self.load()
+
+        # Binding
+        self.canvas.bind('<Button-1>',self.clic)
+
+    def load(self):
+        with open('Map/Map.txt',"r") as f:
+            text=f.read()
+        lines=text.split('\n')
+        batis=lines[-1]
+        lines=lines[0:len(lines)-1]
+        self.grid=[lines[i].split(' ') for i in range(len(lines))]
+        self.x_size=len(self.grid)
+        self.y_size=len(self.grid[0])
+        self.ppc=int(min(self.winfo_screenwidth()/self.y_size,(self.winfo_screenheight()-300)/self.x_size))
+        self.canvas = Canvas(self,width=self.y_size*self.ppc,height=self.x_size*self.ppc,bg='white')
+        self.timer=Label(self,width=self.ppc,text="COUCOU",font=("Arial",20))
+        self.canvas.config(width=self.ppc*self.y_size,height=self.ppc*self.x_size)
+        self.timer.grid(column=0)
+        Button(self,text='launch',command=self.run).grid()
+        self.canvas.grid(column=0)
+        self.nbati=2
+        for i in range (self.x_size):
+            for j in range (self.y_size):
+                self.grid[i][j]=self.grid[i][j].split('/')
+                for k in range (len(self.grid[i][j])):
+                    self.grid[i][j][k]=int(self.grid[i][j][k])
+                if self.grid[i][j][2]==1:
+                    self.color(i,j,'black')
+                elif self.grid[i][j][2]==2:
+                    self.color(i,j,'blue')
+                elif self.grid[i][j][2]==3:
+                    self.color(i,j,'yellow')
+                elif self.grid[i][j][2]==4:
+                    self.color(i,j,'green')
+        batims=batis.split(' ')
+        self.batiments=([b.split('/') for b in batims])
+
+    def color(self,nx,ny,color):
+        self.canvas.create_rectangle(ny*self.ppc,nx*self.ppc,(ny+1)*self.ppc-1,(nx+1)*self.ppc-1,fill=color,outline=color)
+
+    def genSound(self,x0,y0,volume):
+        xMax,yMax=self.x_size,self.y_size
+        if x0<0 or x0>xMax or y0<0 or y0>yMax:
+            print("error")
+            return
+        self.grid[x0][y0][1]+=volume
+        self.color(x0,y0,self.reform(round(self.grid[x0][y0][1])))
+        if volume==1:
+            return
+        moves=[(0,1), (0,-1), (1,0), (-1,0), (-1,-1), (-1,1), (1,-1), (1,1)]
+        suivants=[(x0,y0,volume)]
+
+        visited=[[False for _ in range(2*volume+1)] for _ in range(2*volume+1)]
+        visited[volume][volume]=True
+
+        while suivants:
+            new=[]
+            for a,b,M in suivants:
+                for dx,dy in moves:
+                    x,y=a+dx,b+dy
+                    value=M-(dx**2+dy**2)**(1/2)
+                    if x>=0 and x<xMax and y>=0 and y<yMax and (self.grid[x][y][2]==0 or self.grid[x][y][2]==2)  and not(visited[x-x0+volume][y-y0+volume]) and value>0:
+                        self.grid[x][y][1]+=round(value)
+                        visited[x-x0+volume][y-y0+volume]=True
+                        self.color(x,y,self.reform(self.grid[x][y][1]))
+                        new.append((x,y,value))
+            suivants=new[:]
+
+    def reform(self,n):
+        #num=int(500/numpy.pi*numpy.arctan(0.5*n))
+        num=int(250*(1-numpy.exp(-n/5)))
+        txt=hex(255-num)[2:]
+        if len(txt)==1:
+            txt='0'+txt
+        return('#ff'+txt+txt)
+
+    def plotHumain(self,L):
+        d=int(self.ppc/3.5)
+        for oval in self.humains:
+            self.canvas.delete(oval)
+        self.humains=[]
+        for (x,y) in L:
+            px,py=int(self.ppc*x),int(self.ppc*y)
+            self.humains.append(self.canvas.create_oval(px-d,py-d,px+d,py+d,fill='#ba4a00'))
+
+    def plotZombie(self,L):
+        d=int(self.ppc/3.5)
+        for oval in self.zombies:
+            self.canvas.delete(oval)
+        self.zombies=[]
+        for (x,y) in L:
+            px,py=int(self.ppc*x),int(self.ppc*y)
+            self.zombies.append(self.canvas.create_oval(px-d,py-d,px+d,py+d,fill='#4a235a'))
+
+    def plotPath(self,path):
+        for x,y in path:
+            self.color(x,y,"#aff")
+
+    def clic(self,event):
+        self.genSound(int(event.y/self.ppc),int(event.x/self.ppc),5)
+
+    def run(self):
+        with open("Journal/Sauvegarde.txt","r") as f:
+            text=f.read()
+        turns=text.split("***\n")
+        def go(t):
+            Lh,Lz=[],[]
+            lines=turns[t].split("\n")
+            self.timer.config(text=str(t))
+            debut,fin=1,int(lines[0])+1
+            for i in range(debut,fin):
+                humain=lines[i].split("/")
+                Lh.append((float(humain[0]),float(humain[1])))
+            debut,fin=fin+1,fin+1+int(lines[fin])
+            for i in range(debut,fin):
+                zombie=lines[i].split("/")
+                Lz.append((float(zombie[0]),float(zombie[1])))
+            debut,fin=fin+1,fin+1+int(lines[fin])
+            for i in range(debut,fin):
+                sound=lines[i].split("/")
+                self.genSound(int(sound[0]),int(sound[1]),int(sound[2]))
+            self.plotHumain(Lh)
+            self.plotZombie(Lz)
+            if t<len(turns)-1:
+                self.after(1000,lambda: go(t+1))
+        go(0)
+
+E=Visualisator()
+E.focus_force()
+E.mainloop()
---
+++ Simulation/mapeditor.py
@@ -0,0 +1,244 @@
+from tkinter import *
+
+def line(x1,y1,x2,y2):
+
+    if x1==x2:
+        y1,y2=min(y1,y2),max(y1,y2)
+        return([(x1, y) for y in range(y1, y2+1)])
+    if y1==y2:
+        x1,x2=min(x1,x2),max(x1,x2)
+        return([(x, y1) for x in range(x1, x2+1)])
+
+    L=[(x1,y1)]
+    x,y=x1,y1
+    dx=(x2-x1)//abs(x2-x1)
+    inverse=False
+    if y2<y1:
+        y2=y1+abs(y2-y1)
+        inverse=True
+    a=(y2-y1)/(x2-x1)
+    while x!=x2 or y!=y2:
+        if y<=a*(x-x1)+y1:
+            y+=1
+        else:
+            x+=dx
+        L.append((x,y))
+    if inverse:
+        for i in range(len(L)):
+            x,y=L[i]
+            L[i]=(x,y1-(y-y1))
+    return(L)
+
+
+
+class Map_editor(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+
+        # Data
+        self.x_size=10    # In cells
+        self.y_size=10    # In cells
+        self.ppc=int(min((self.winfo_screenwidth()-300)/self.y_size,self.winfo_screenheight()/self.x_size))
+        self.onclic='w'    # w : wall, wl : line of wall, b : building, f : food, r : rest
+        self.nclic = 1     # first or second clic for a new wall of line
+        self.firstcase = (0,0)     # first clic for a wall of line
+        self.grid=[[[0,0,0] for _ in range (self.y_size)] for _ in range (self.x_size)]
+        self.nbati=1
+        self.batiments=[]
+        # Each cell is [x,y,z] where y is noise level and z is wall/not wall
+
+        #Canvas
+        self.canvas = Canvas(self,width=self.y_size*self.ppc,height=self.x_size*self.ppc,bg='white')
+        self.canvas.grid(row=0,column=0,rowspan=10)
+
+        #Save/Open
+        self.frame1 = Frame(self)
+        self.filename_label = Label(self.frame1,text='Nom de la carte (no file extension)')
+        self.filename_entry = Entry(self.frame1)
+        self.frame2 = Frame(self)
+        self.finish_button = Button(self.frame2,text='Sauvegarder',command=self.finish)
+        self.open_button = Button(self.frame2,text='Ouvrir',command=self.load)
+        self.clear_button = Button(self.frame2,text='Nettoyer',command=self.clear)
+        self.frame3 = Frame(self)
+        self.onclic_label = Label(self.frame3,text='Cliquer pour ajouter un mur')
+        self.onclicwall_button = Button(self.frame3,text='Mur au clic',command=lambda: self.setclic('w'))
+        self.onclicline_button = Button(self.frame3,text='Ligne au clic',command=lambda: self.setclic('wl'))
+        self.onclicbuild_button = Button(self.frame3,text='Batiment au clic',command=lambda: self.setclic('b'))
+        self.onclicfood_button = Button(self.frame3,text='Zone food au clic',command=lambda: self.setclic('f'))
+        self.onclicrest_button = Button(self.frame3,text='Zone repos au clic',command=lambda: self.setclic('r'))
+        self.onclicclear_button= Button(self.frame3,text='Nettoyerbis au clic',command=lambda: self.setclic('clb'))
+
+        #Layout
+        self.frame1.grid(row=0,column=1)
+        self.filename_label.grid(row=0,column=0)
+        self.filename_entry.grid(row=1,column=0)
+
+        self.frame2.grid(row=1,column=1)
+        self.finish_button.grid(row=0,column=0)
+        self.open_button.grid(row=1,column=0)
+        self.clear_button.grid(row=2,column=0)
+
+        self.frame3.grid(row=2,column=1)
+        Label(self.frame3,text='------').grid(row=0,column=0)
+        self.onclic_label.grid(row=1,column=0)
+        self.onclicwall_button.grid(row=2,column=0)
+        self.onclicline_button.grid(row=3,column=0)
+        self.onclicbuild_button.grid(row=4,column=0)
+        self.onclicfood_button.grid(row=5,column=0)
+        self.onclicrest_button.grid(row=6,column=0)
+        self.onclicclear_button.grid(row=7,column=0)
+
+        # Binding
+        self.canvas.bind('<Button-1>',self.clic)
+        self.canvas.bind('<Button-3>',self.rclic)
+        self.bind('Escape',self.leave)
+
+    def clic(self,event):
+        nx,ny=event.x//self.ppc,event.y//self.ppc
+        if self.onclic == 'w':
+            self.add_wall(ny,nx)
+        elif self.onclic == 'wl':
+            if self.nclic == 1:
+                self.firstcase = (ny,nx)
+                self.color(ny,nx,"grey")
+                self.nclic+=1
+            else:
+                self.add_line(self.firstcase[0],self.firstcase[1],ny,nx)
+                self.nclic-=1
+        elif self.onclic == 'b':
+            if self.nclic == 1:
+                self.firstcase = (ny,nx)
+                self.nclic+=1
+            elif self.nclic == 2:
+                self.nclic +=1
+                self.add_build(self.firstcase[0],self.firstcase[1],ny,nx)
+            else:
+                self.adddoor(ny,nx)
+        elif self.onclic == 'f':            # food zone on cilc
+            self.grid[ny][nx][2]=3
+            self.color(ny,nx,'yellow')
+        elif self.onclic == 'r':            # rest zone on cilc
+            self.grid[ny][nx][2]=4
+            self.color(ny,nx,'green')
+        elif self.onclic == 'clb':
+            self.grid[ny][nx][2]=0
+            self.color(ny,nx,'white')
+
+    def rclic(self,event):
+        self.nclic = 1
+        self.nbati += 1
+
+    def add_line(self,nx1,ny1,nx2,ny2):
+        L=line(nx1,ny1,nx2,ny2)
+        for case in L:
+            self.add_wall(case[0],case[1])
+
+    def add_wall(self,nx,ny):
+        self.grid[nx][ny][2]=1
+        self.color(nx,ny,'black')
+
+    def add_wallinbati(self,nx,ny):
+        self.grid[nx][ny][2]=1
+        self.grid[nx][ny][0]=self.nbati
+        self.color(nx,ny,'black')
+
+    def adddoor(self,nx,ny):
+        if self.grid[nx][ny][0] == self.nbati:
+            self.grid[nx][ny][2]=2
+            self.color(nx,ny,'blue')
+        self.batiments[self.nbati-1].append(nx)
+        self.batiments[self.nbati-1].append(ny)
+
+    def add_build(self,nx1,ny1,nx2,ny2):
+        for nx in range (min(nx1,nx2),max(nx1,nx2)+1):
+            self.add_wallinbati(nx,min(ny1,ny2))
+            self.add_wallinbati(nx,max(ny1,ny2))
+        for ny in range (min(ny1,ny2),max(ny1,ny2)+1):
+            self.add_wallinbati(min(nx1,nx2),ny)
+            self.add_wallinbati(max(nx1,nx2),ny)
+        self.batiments.append([nx1,ny1,nx2,ny2])
+
+    def color(self,nx,ny,color):
+        self.canvas.create_rectangle(ny*self.ppc,nx*self.ppc,(ny+1)*self.ppc-1,(nx+1)*self.ppc-1,fill=color,outline=color)
+
+    def finish(self):
+        filename='Map/'+self.filename_entry.get()
+        if filename != '':
+            g=self.grid
+            with open(filename+'.txt',"w") as f:
+                for i in range(len(g)):
+                    for j in range(len(g[0])-1):
+                        f.write(str(g[i][j][0])+'/'+str(g[i][j][1])+'/'+str(g[i][j][2])+' ')
+                    f.write(str(g[i][len(g[0])-1][0])+'/'+str(g[i][len(g[0])-1][1])+'/'+str(g[i][len(g[0])-1][2])+'\n')
+                for i in range (len(self.batiments)):
+                    for j in range (len(self.batiments[i])-1):
+                        f.write(str(self.batiments[i][j])+'/')
+                    f.write(str(self.batiments[i][-1]))
+                    if i!=len(self.batiments)-1:
+                        f.write(' ')
+
+    def load(self):
+        filename='Map/'+self.filename_entry.get()
+        try:
+            with open(filename+'.txt',"r") as f:
+                text=f.read()
+            lines=text.split('\n')
+            batis=lines[-1]
+            lines=lines[0:len(lines)-1]
+            self.grid=[lines[i].split(' ') for i in range(len(lines))]
+            self.canvas.delete("all")
+            self.canvas.config(width=self.ppc*len(self.grid[0]),height=self.ppc*len(self.grid))
+            self.nbati=1
+            for i in range (len(self.grid)):
+                for j in range (len(self.grid[0])):
+                    self.grid[i][j]=self.grid[i][j].split('/')
+                    for k in range (len(self.grid[i][j])):
+                        self.grid[i][j][k]=int(self.grid[i][j][k])
+                    if self.grid[i][j][2]==1:
+                        self.color(i,j,'black')
+                    elif self.grid[i][j][2]==2:
+                        self.color(i,j,'blue')
+                    elif self.grid[i][j][2]==3:
+                        self.color(i,j,'yellow')
+                    elif self.grid[i][j][2]==4:
+                        self.color(i,j,'green')
+            batims=batis.split(' ')
+            self.batiments=[b.split('/') for b in batims]
+        except:
+            pass
+
+    def clear(self):
+        self.grid=[[[0,0,0] for _ in range (self.y_size)] for _ in range (self.x_size)]
+        self.nclic=1
+        self.onclic = 'w'    # wl for wall line
+        self.onclic_label.config(text='Cliquer pour ajouter un mur')
+        self.canvas.delete("all")
+        self.batiments=[]
+
+    def setclic(self,txt):  # change from one wall per clic to a line of wall per two clics
+        self.nclic=1
+        if txt == 'w':
+            self.onclic = 'w'
+            self.onclic_label.config(text='Cliquer pour ajouter un mur')
+        elif txt == 'wl':
+            self.onclic = 'wl'
+            self.onclic_label.config(text='Cliquer deux fois pour \n une ligne de murs')
+        elif txt == 'b':
+            self.onclic = 'b'
+            self.onclic_label.config(text='Cliquer deux fois pour \n un batiment, puis une \n fois pour la porte, \n puis clic droit')
+        elif txt == 'f':
+            self.onclic = 'f'
+            self.onclic_label.config(text='Cliquer pour ajouter \n une case nourriture')
+        elif txt == 'clb':
+            self.onclic = 'clb'
+            self.onclic_label.config(text='Cliquer pour nettoyer un point')
+        if txt == 'r':
+            self.onclic = 'r'
+            self.onclic_label.config(text='Cliquer pour ajouter \n une zone de repos')
+
+    def leave(self,event):
+        self.destroy()
+
+E=Map_editor()
+E.focus_force()
+E.mainloop()
