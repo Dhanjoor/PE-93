92c4533b6301f10019e35c020cd6200f92633657
tazalapizza <44235349+tazalapizza@users.noreply.github.com>
2019-03-27 17:19:24 +0100

    minor changes

--- Simulation/CreateBeing.py
+++ Simulation/CreateBeing.py
@@ -2,14 +2,15 @@ from random import random, randint
 from Parameters import *
 from Being import *
 
-def create_zombie(Master):
+def createZombie(Master):
     x,y=randint(0, xSize-1), randint(0, ySize-1)
     while Master.Map[x][y].content!=0 or Master.Map[x][y].idBuilding!=0:
         x,y=randint(0, xSize-1), randint(0, ySize-1)
+    x,y=x+random(),y+random()
 
-    return(Zombie(Master, (x,y)))
+    return(Zombie(Master, [x,y]))
 
-def create_human(Master):
+def createHuman(Master):
     ability=random()
     if ability<=pAbilities[0]:
         ability=weak
@@ -20,30 +21,31 @@ def create_human(Master):
 
     morality=random()
     if morality<=pMoralities[0]:
-        morality=evil
+        morality="evil"
     elif morality<=pMoralities[1]+pMoralities[0]:
-        morality=neutral
+        morality="neutral"
     else:
-        morality=hero
+        morality="hero"
 
     behavior=random()
     if behavior<=pBehaviors[0]:
-        behavior=flee
+        behavior="flee"
     elif behavior<=pBehaviors[1]+pBehaviors[0]:
-        behavior=hide
+        behavior="hide"
     else:
-        behavior=fight
+        behavior="fight"
 
     coldblood=random()
     if coldblood<=pStress[0]:
-        coldblood=zen
+        coldblood="zen"
     elif coldblood<=pStress[1]+pStress[0]:
-        coldblood=stable
+        coldblood="stable"
     else:
-        coldblood=stressed
+        coldblood="stressed"
 
     x,y=randint(0, xSize-1), randint(0, ySize-1)
     while Master.Map[x][y].content!=0:
         x,y=randint(0, xSize-1), randint(0, ySize-1)
+    x,y=x+random(),y+random()
 
-    return(Human(Master, (x,y),ability[0], ability[1], ability[2], coldblood, morality, behavior))
+    return(Human(Master, [x,y],ability[0], ability[1], ability[2], coldblood, morality, behavior))
