c3fcc60c407e086affc57e0cf96de9efbb4ea94d
tazalapizza <44235349+tazalapizza@users.noreply.github.com>
2019-04-03 15:47:59 +0200

    pathfinding

--- Simulation/Being.py
+++ Simulation/Being.py
@@ -187,6 +187,51 @@ class Human(Being):
         self.Master.Zombies.append(Zombie(self.Master,self.position))             #creating a new zombie
         self.death()
 
+    def pathfinding(self,xd,yd): # xh,yh position of human, xd,yd position of door
+        
+        # We create a table of cells that can be passed through
+        m=max(xSize,ySize)**2
+        Access=[[m for _ in range(ySize)] for _ in range(xSize)]
+        for i in range(xSize):
+            for j in range(ySize):
+                if self.Master.Map[i][j].content:
+                    Access[i][j] = -1
+        for zombie in self.Master.Zombies :
+            x,y=zombie.cell
+            Access[x][y] = -1
+            
+        # All possible Moves to a neighbor cell, list of cells to visit, final path
+        Moves=[(1,0),(0,1),(-1,0),(0,-1)]
+        Tovisit=[(self.position[0],self.position[1])]
+        Access[self.position[0]][self.position[1]]=0
+        
+        # BFS
+        fini=False
+        while len(Tovisit)>0 and not(fini):
+            x,y=Tovisit.pop()
+            n=Access[x][y]
+            for dx,dy in Moves:
+                x2,y2=x+dx,y+dy
+                if (0<=x2 and x2<xSize and 0<=y2 and y2<ySize and Access[x2][y2]>n+1):
+                    Tovisit.insert(0,(x2,y2))
+                    Access[x2][y2]=n+1
+                if (x2,y2)==(xd,yd):
+                    fini=True
+                    break
+                
+        # Extraction of the path
+        if fini:
+            Path,x,y,n=[(xd,yd)],xd,yd,Access[xd][yd]
+            while n>0:
+                for dx,dy in Moves:
+                    if (0<=x+dx and x+dx<=xSize-1 and 0<=y+dy and y+dy<=ySize-1 and Access[x+dx][y+dy]==n-1):
+                        x,y,n=x+dx,y+dy,n-1
+                        Path.insert(0,(x,y))
+                        break
+            return Path
+        else:
+            return ([])        
+        
     def fight(self):
         self.Master.genSound(self.cell[0],self.cell[1], fightVolume)
         Zstrength=0
